29e06992af231dd1ab73b8ba888e5ea5d08fa51e
rodoviti <rodolfo.viti@everis.com>
2020-05-25 23:27:51 +0200

    fix feign client test cases

--- integration/rest/src/test/java/it/gov/pagopa/bpd/notification_manager/connector/NotificationRestClientTest.java
+++ integration/rest/src/test/java/it/gov/pagopa/bpd/notification_manager/connector/NotificationRestClientTest.java
@@ -1,73 +1,77 @@
 package it.gov.pagopa.bpd.notification_manager.connector;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import it.gov.pagopa.bpd.common.connector.BaseFeignRestClientTest;
 import it.gov.pagopa.bpd.notification_manager.connector.config.BpdNotificationManagerRestConnectorConfig;
 import it.gov.pagopa.bpd.notification_manager.connector.model.NotificationDTO;
 import it.gov.pagopa.bpd.notification_manager.connector.model.NotificationResource;
+import lombok.SneakyThrows;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.TestPropertySourceUtils;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 @TestPropertySource(
         locations = "classpath:config/rest-client.properties",
         properties = "spring.application.name=bpd-ms-notification-manager-integration-rest")
-@Import({BpdNotificationManagerRestConnectorConfig.class})
+@ContextConfiguration(initializers = NotificationRestClientTest.RandomPortInitializer.class,
+        classes = BpdNotificationManagerRestConnectorConfig.class)
 public class NotificationRestClientTest extends BaseFeignRestClientTest {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private NotificationRestClient restClient;
-
-    static {
-        SERIVICE_PORT_ENV_VAR_NAME = "URL_BACKEND_IO_PORT";
-    }
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(wireMockConfig()
+            .dynamicPort()
+            .usingFilesUnderClasspath("stubs/notification")
+    );
 
     @Test
-    public void notify_test() throws JsonProcessingException {
-        final NotificationResource expectedResponse = new NotificationResource();
-        expectedResponse.setMessage("ok");
-        final String fiscalCode = "test";
-        final String TOKEN = "token";
-        final NotificationDTO notificationDTO = new NotificationDTO();
-        final NotificationDTO.NotificationMessage message = new NotificationDTO.NotificationMessage();
-        final NotificationDTO.SenderMetadata senderMetadata = new NotificationDTO.SenderMetadata();
+    public void notify_test() {
+        final String token = "token";
         final NotificationDTO.Content content = new NotificationDTO.Content();
         content.setSubject("subject");
         content.setMarkdown("markdown");
-        Instant nowUtc = Instant.now();
-        ZoneId zoneId = ZoneId.of("Europe/Paris");
-        ZonedDateTime nowDateTime = ZonedDateTime.ofInstant(nowUtc, zoneId);
+        final NotificationDTO.NotificationMessage message = new NotificationDTO.NotificationMessage();
         message.setId("id");
-        message.setFiscal_code(fiscalCode);
-        message.setCreated_at(nowDateTime.toString());
-        message.setContent(content);
+        message.setFiscal_code("test");
+        message.setCreated_at("2020-04-17T12:23:00.749+02:00");
         message.setSender_service_id("serviceId");
+        message.setContent(content);
+        final NotificationDTO.SenderMetadata senderMetadata = new NotificationDTO.SenderMetadata();
         senderMetadata.setService_name("serviceName");
         senderMetadata.setOrganization_name("organizationName");
         senderMetadata.setDepartment_name("departmentName");
+        final NotificationDTO notificationDTO = new NotificationDTO();
         notificationDTO.setMessage(message);
         notificationDTO.setSender_metadata(senderMetadata);
-        stubFor(post(urlEqualTo("/notify?TOKEN=token"))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE)
-                        .withBody(objectMapper.writeValueAsString(expectedResponse))));
 
-        final NotificationResource actualResponse = restClient.notify(notificationDTO, TOKEN);
-        assertEquals(expectedResponse, actualResponse);
+        final NotificationResource actualResponse = restClient.notify(notificationDTO, token);
+
+        assertNotNull(actualResponse);
+        assertEquals("ok", actualResponse.getMessage());
+    }
+
+    @Autowired
+    private NotificationRestClient restClient;
+
+    public static class RandomPortInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @SneakyThrows
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertySourceUtils
+                    .addInlinedPropertiesToEnvironment(applicationContext,
+                            String.format("rest-client.notification.base-url=http://%s:%d",
+                                    wireMockRule.getOptions().bindAddress(),
+                                    wireMockRule.port())
+                    );
+        }
     }
 }
