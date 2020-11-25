69dc3e06f430facaea38e10452c6ca8b79629f2c
alecastagno <alessandro.castagno.sia@gmail.com>
2020-11-25 19:15:57 +0100

    add csv encryption

--- core/src/main/java/it/gov/pagopa/bpd/notification_manager/service/NotificationServiceImpl.java
+++ core/src/main/java/it/gov/pagopa/bpd/notification_manager/service/NotificationServiceImpl.java
@@ -10,16 +10,17 @@ import it.gov.pagopa.bpd.notification_manager.connector.io_backend.model.Notific
 import it.gov.pagopa.bpd.notification_manager.connector.io_backend.model.NotificationResource;
 import it.gov.pagopa.bpd.notification_manager.connector.jpa.CitizenDAO;
 import it.gov.pagopa.bpd.notification_manager.connector.jpa.model.WinningCitizen;
+import it.gov.pagopa.bpd.notification_manager.encryption.EncryptUtil;
 import it.gov.pagopa.bpd.notification_manager.mapper.NotificationDtoMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -47,13 +48,17 @@ class NotificationServiceImpl extends BaseService implements NotificationService
     private final String DELIMITER;
     private final Long maxRow;
     private final String serviceName;
+    private final String authorityType;
     private final String fileType;
+    private final String publicKey;
 
     @Autowired
     NotificationServiceImpl(
-            CitizenDAO citizenDAO, NotificationDtoMapper notificationDtoMapper,
+            CitizenDAO citizenDAO,
+            NotificationDtoMapper notificationDtoMapper,
             NotificationRestConnector notificationRestConnector,
-            AwardPeriodRestClient awardPeriodRestClient, WinnersSftpConnector winnersSftpConnector,
+            AwardPeriodRestClient awardPeriodRestClient,
+            WinnersSftpConnector winnersSftpConnector,
             @Value("${core.NotificationService.updateRankingAndFindWinners.outputFilePath}") String outputFilePath,
             @Value("${core.NotificationService.notifyUnsetPayoffInstr.ttl}") Long timeToLive,
             @Value("${core.NotificationService.notifyUnsetPayoffInstr.subject}") String subject,
@@ -61,7 +66,9 @@ class NotificationServiceImpl extends BaseService implements NotificationService
             @Value("${core.NotificationService.notifyWinners.delimiter}") String delimiter,
             @Value("${core.NotificationService.notifyWinners.maxRow}") Long maxRow,
             @Value("${core.NotificationService.notifyWinners.serviceName}") String serviceName,
-            @Value("${core.NotificationService.notifyWinners.fileType}") String fileType) {
+            @Value("${core.NotificationService.notifyWinners.authorityType}") String authorityType,
+            @Value("${core.NotificationService.notifyWinners.fileType}") String fileType,
+            @Value("${core.NotificationService.notifyWinners.publicKey}") String publicKey) {
         this.citizenDAO = citizenDAO;
         this.notificationDtoMapper = notificationDtoMapper;
         this.notificationRestConnector = notificationRestConnector;
@@ -74,7 +81,9 @@ class NotificationServiceImpl extends BaseService implements NotificationService
         DELIMITER = delimiter;
         this.maxRow = maxRow;
         this.serviceName = serviceName;
+        this.authorityType = authorityType;
         this.fileType = fileType;
+        this.publicKey = publicKey;
     }
 
 
@@ -118,7 +127,28 @@ class NotificationServiceImpl extends BaseService implements NotificationService
             if (logger.isInfoEnabled()) {
                 logger.info("NotificationManagerServiceImpl.findWinners");
             }
-            List<AwardPeriod> activePeriods = awardPeriodRestClient.findActiveAwardPeriods();
+            //List<AwardPeriod> activePeriods = awardPeriodRestClient.findActiveAwardPeriods();
+            //TODO rimuovere blocco --->
+            List<AwardPeriod> activePeriods = new ArrayList<AwardPeriod>();
+            AwardPeriod awp1 = new AwardPeriod();
+            awp1.setAwardPeriodId(2L);
+            awp1.setEndDate(LocalDate.now().minus(Period.ofDays(15)));
+            awp1.setGracePeriod(15L);
+            awp1.setStartDate(LocalDate.now().minus(Period.ofDays(50)));
+            activePeriods.add(awp1);
+            AwardPeriod awp2 = new AwardPeriod();
+            awp2.setAwardPeriodId(1L);
+            awp2.setEndDate(LocalDate.now().minus(Period.ofDays(5)));
+            awp2.setGracePeriod(15L);
+            awp2.setStartDate(LocalDate.now().minus(Period.ofDays(40)));
+            activePeriods.add(awp2);
+            AwardPeriod awp3 = new AwardPeriod();
+            awp3.setAwardPeriodId(3L);
+            awp3.setEndDate(LocalDate.now().plus(Period.ofDays(5)));
+            awp3.setGracePeriod(15L);
+            awp3.setStartDate(LocalDate.now().minus(Period.ofDays(15)));
+            activePeriods.add(awp3);
+            // <---
             Long endingPeriodId = null;
             for (AwardPeriod activePeriod : activePeriods) {
                 if (activePeriod.getEndDate().plus(Period.ofDays(activePeriod.getGracePeriod().intValue()))
@@ -137,7 +167,7 @@ class NotificationServiceImpl extends BaseService implements NotificationService
                     List<String> dataLines = new ArrayList<>();
                     int m = 0;
                     int n = 0;
-                    String fileName = "testCSV_";
+                    Path tempDir = Files.createTempDirectory("csv_directory");
                     for (WinningCitizen winnerForCSV : winnersForCSV) {
                         n++;
                         String paymentReason = (winnerForCSV.getAccountHolderFiscalCode().equals(winnerForCSV.getFiscalCode())) ?
@@ -173,19 +203,29 @@ class NotificationServiceImpl extends BaseService implements NotificationService
                             DecimalFormat twoDigits = new DecimalFormat("00");
                             String currentFileNumber = twoDigits.format(m);
                             String totalFileNumber = twoDigits.format((int) Math.ceil((double) winnersForCSV.size() / maxRow));
-//                            TODO definire fileType
-                            File csvOutputFile = new File(serviceName + "."
+
+                            File csvOutputFile = new File(tempDir + "\\"
+                                    + serviceName + "."
+                                    + authorityType + "."
                                     + fileType + "."
                                     + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) + "."
-                                    + LocalTime.now().format(DateTimeFormatter.ofPattern("hhmmss"))+ "."
-                                    + currentFileNumber + "_" + totalFileNumber
+                                    + LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss")) + "."
+                                    + currentFileNumber + "_" + totalFileNumber + "."
+                                    + dataLines.size()
                                     + ".csv");
-//                            TODO rimuovere commento per produrre il csv
-//                            try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-//                                dataLines.forEach(pw::println);
-//                            }
 
-//                            TODO cifrare csv e inviare a sftp (decommentare)
+                            try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+                                dataLines.forEach(pw::println);
+                            }
+                            String publicKeyWithLineBreaks = publicKey.replace("\\n", System.lineSeparator());
+                            InputStream publicKeyIS = new ByteArrayInputStream(publicKeyWithLineBreaks.getBytes());
+                            FileOutputStream outputFOS = new FileOutputStream(csvOutputFile.getAbsolutePath().concat(".pgp"));
+                            EncryptUtil.encryptFile(outputFOS,
+                                    csvOutputFile.getAbsolutePath(),
+                                    EncryptUtil.readPublicKey(publicKeyIS),
+                                    false, true);
+
+//                            TODO inviare a sftp (decommentare)
 //                            winnersSftpConnector.sendFile(csvOutputFile);
                             dataLines.clear();
                         }
